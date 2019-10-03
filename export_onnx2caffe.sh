68cd4d9007bb239dd1e1017860d9f9813d30a5f2
xuanphu94 <nxphu.1994@gmail.com>
2019-10-03 10:55:50 +0700

    update

--- export_onnx2caffe.sh
+++ export_onnx2caffe.sh
@@ -1,5 +1,5 @@
 export CAFFE_ROOT=/home/cybercore/sw/caffe/python
 export PYTHONPATH=/home/cybercore/sw/caffe/python
 
-ONNX_FILE="/home/cybercore/thuync/checkpoints/denet85/swapped_head_denet85.onnx"
+ONNX_FILE="/home/cybercore/thuync/checkpoints/denet56/denet56_swaphead_ratiofirst.onnx"
 python3 convertCaffe.py --onnx_file ${ONNX_FILE}
