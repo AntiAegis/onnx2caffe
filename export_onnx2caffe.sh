18ff7b5ea9465b2e329433f8c3d0481378bb7c3f
AntiAegis <thuypn9a4@gmail.com>
2019-10-30 11:26:38 +0700

    update

--- export_onnx2caffe.sh
+++ export_onnx2caffe.sh
@@ -1,5 +1,5 @@
-export CAFFE_ROOT=/home/cybercore/sw/caffe/python
-export PYTHONPATH=/home/cybercore/sw/caffe/python
+export CAFFE_ROOT=/home/thuync/caffe/python
+export PYTHONPATH=/home/thuync/caffe/python
 
-ONNX_FILE="/home/cybercore/thuync/checkpoints/denet56/denet56_swaphead_ratiofirst.onnx"
+ONNX_FILE="/home/thuync/thuync/checkpoints/ObjDet/denet511/denet511.onnx"
 python3 convertCaffe.py --onnx_file ${ONNX_FILE}
